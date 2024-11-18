d86e1e2f85cfbf5ed82517e594e5bdbdf917b58c
Shauna Mack <shaunakatemack@gmail.com>
2024-11-18 13:30:03 +1000

    add a colour change to the button when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
+  // set the message that will be rendered on the page
   const [message, setMessage] = React.useState("JavaScript is so cool.");
+
+  // toggle colour of heading
+  const [isColorToggled, setIsColorToggled] = React.useState(false);
 
   function handleClick() {
     setMessage((prevMessage) =>
@@ -9,13 +13,14 @@ function App() {
         ? "I love React"
         : "JavaScript is so cool."
     );
+    setIsColorToggled(!isColorToggled);
   }
 
   // the return is what is rendered on the page
   return (
     // in React we have to have 1 wrapper element
     <>
-      <h1>{message}</h1>
+      <h1 style={{ color: isColorToggled ? "pink" : "purple" }}>{message}</h1>
       <button onClick={handleClick}>Update the message!</button>
     </>
   );
