287f76a69b471f0b7435cc46a0c4a4e21fd32f45
Shauna Mack <shaunakatemack@gmail.com>
2024-11-18 13:27:42 +1000

    set the state of the message and allow it to change on a button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import React from "react";
+
 function App() {
-  const message = "React, here we go!";
-  return <h1>{message}</h1>;
+  const [message, setMessage] = React.useState("JavaScript is so cool.");
+
+  function handleClick() {
+    setMessage((prevMessage) =>
+      prevMessage === "JavaScript is so cool."
+        ? "I love React"
+        : "JavaScript is so cool."
+    );
+  }
+
+  // the return is what is rendered on the page
+  return (
+    // in React we have to have 1 wrapper element
+    <>
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Update the message!</button>
+    </>
+  );
 }
 
 export default App;
